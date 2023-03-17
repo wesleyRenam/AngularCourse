c7a0a12861bab55be39b5b6fc244db4efd0ff031
wesleyRenam <wesleyrenamjob@gmail.com>
2023-03-17 10:46:40 -0300

    Day 5 of Angular Course

---
+++ recipeBook-Project-1/src/app/recipes/recipes-list/recipes-list.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-recipes-list',
+  templateUrl: './recipes-list.component.html',
+  styleUrls: ['./recipes-list.component.css']
+})
+export class RecipesListComponent {
+  recipes = [];
+
+}
