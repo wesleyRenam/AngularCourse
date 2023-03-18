fd5941df124bea70155966d8bd5b0706cbf315e6
wesleyRenam <wesleyrenam@gmail.com>
2023-03-18 10:53:22 -0300

    Day 6 of Angular Course

--- recipeBook-Project-1/src/app/recipes/recipes-list/recipes-list.component.ts
+++ recipeBook-Project-1/src/app/recipes/recipes-list/recipes-list.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipes-list',
@@ -6,6 +7,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./recipes-list.component.css']
 })
 export class RecipesListComponent {
-  recipes = [];
+  recipes: Recipe[] = [
+    new Recipe("A simple test recipe", "Description test", "https://i2.wp.com/files.agro20.com.br/uploads/2020/03/comidabrasileira3.jpg?fit=1024%2C585&ssl=1")
+  ];
+
+  constructor() {
+
+  }
+
+  ngOnInit() {
+
+  }
 
 }
--- recipeBook-Project-1/src/app/shopping-list/shopping-list.component.ts
+++ recipeBook-Project-1/src/app/shopping-list/shopping-list.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Ingredient } from '../shared/ingredient.model';
 
 @Component({
   selector: 'app-shopping-list',
@@ -6,5 +7,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./shopping-list.component.css']
 })
 export class ShoppingListComponent {
+  ingredients: Ingredient[] = [
+    new Ingredient('Tomato', 5),
+    new Ingredient('Apples', 10),
+  ];
+
 
 }
